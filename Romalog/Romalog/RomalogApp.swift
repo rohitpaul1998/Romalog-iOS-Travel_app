3faab6b439e88ffbe47cb91398b9d81665a8e0fe
Rohit Paul Gerard Nagarajan <gerardnagarajan.r@northeastern.edu>
2024-04-15 23:15:30 -0400

    Commit 1 - Pushing initial project env

---
+++ Romalog/Romalog/RomalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  RomalogApp.swift
+//  Romalog
+//
+//  Created by Rohit Paul on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct RomalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
