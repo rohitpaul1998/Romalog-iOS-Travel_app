be9e0a52b7d005cad76460e83495ce070bc9efed
Rohit Paul Gerard Nagarajan <gerardnagarajan.r@northeastern.edu>
2024-04-22 19:04:22 -0400

    Commit 3 - pushing AddPlaceViewModel code

---
+++ Romalog/Romalog/ViewModels/AddPlaceVM.swift
@@ -0,0 +1,43 @@
+//
+//  AddPlaceVM.swift
+//  Romalog
+//
+//  Created by Rohit Paul on 4/22/24.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+class AddPlaceVM: ObservableObject {
+    @Published var name: String = ""
+    @Published var city: String = ""
+    @Published var country: String = ""
+    @Published var notes: String = ""
+    
+    // progress properties
+    @Published var showProgress = false
+    
+    // image related properties
+    @Published var image: Image?
+    @Published var imageData: Data?
+    
+    func savePlace() {
+        // coredata manager save place logic
+    }
+    
+    func getImageFor(placeName: String) async {
+        let encodedName = placeName.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)!
+        let url = URL(string: "https://source.unsplash.com//1080x1350/?\(encodedName)")!
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            await MainActor.run {
+                imageData = data
+                image = Image(uiImage: UIImage(data: data)!)
+            }
+        } catch let err {
+            print(err.localizedDescription)
+        }
+    }
+}
--- Romalog/Romalog/ViewModels/PlacesListVM.swift
+++ Romalog/Romalog/ViewModels/PlacesListVM.swift
@@ -10,5 +10,17 @@ import SwiftUI
 import UIKit
 
 class PlacesListVM: ObservableObject {
+    @Published var places = [PlaceVM]()
+    @Published var showAddSheet = false
     
+    init() {
+        fetchPlaces()
+    }
+    
+    func fetchPlaces() {
+        places = PlaceVM.sampleData
+        
+        // coredata manager
+        
+    }
 }
